6909222cc712890950cace5539ea6fbb479eca5b
Kamiethenerd <Kamiethenerd@gmail.com>
2015-08-31 17:08:42 -0500

    added post route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var memes = require('./routes/memes');
 
+
 // express init
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // use routes to serve stuff
 app.use('/', index);
 app.use('/memes', memes);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/memes.js
+++ routes/memes.js
@@ -3,10 +3,24 @@ var router = express.Router();
 var fs = require('fs');
 var path = require('path');
 var json = require('../models/memes');
+var messages =require('../models/messages');
+var parser = require('body-parser');
+
 
 // pulls JSON data
 router.get('/', function(req, res, next){
-    res.render('memes', {title: 'Haters Gonna Hate', haters: json});
+    console.log(messages,json);
+    res.render('memes', {title: 'Haters Gonna Hate', haters: json, messages:messages});
+
+});
+
+router.post('/', function(req, res, next){
+    console.log("sending message");
+    var id = req.body.id;
+    var newMessage = req.body.newMessage;
+
+    res.send(id + ", " + newMessage);
+
 });
 
 module.exports = router;
