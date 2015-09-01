44f6395ba20b017a63a796506f8749ae6af4ed33
Kamiethenerd <Kamiethenerd@gmail.com>
2015-09-01 10:05:36 -0500

    routing and appending
    got routes working and appending to the JSON file

--- routes/memes.js
+++ routes/memes.js
@@ -14,10 +14,45 @@ router.get('/', function(req, res, next){
 
 });
 
-router.post('/', function(req, res, next){
+var file = path.join(__dirname,"../models/messages.JSON");
+var newArray;
+
+fs.readFile(file, 'utf8', function (err, data) {
+    if (err) return console.log(err);
+    var msgJson = JSON.parse(data);
+    console.log(msgJson);
+    newArray = msgJson;
+    console.log(newArray);
+    return newArray;
+});
+
+
+router.post('/', function(req, res, next) {
+
+
+
     console.log("sending message");
+
+
     var id = req.body.id;
     var newMessage = req.body.newMessage;
+    //var obj = {name: id, message: newMessage};
+    //var newJson = msgJson.push(obj);
+    //console.log(newJson);
+
+    for (i = 0; i < newArray.length; i++) {
+
+        if (id == newArray[i].name) {
+            newArray[i].message = newArray[i].message + "\n\n" + newMessage;
+        }
+    }
+
+
+    fs.writeFile(file, JSON.stringify(newArray) , 'utf-8', function (err) {
+        if (err) return console.log(err);
+        console.log('Wrote Data');
+    });
+
 
     res.send(id + ", " + newMessage);
 
