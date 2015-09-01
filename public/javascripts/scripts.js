9edd0fd7ac2c074252e543b113f40b28109f93a2
Jason Spidle <jason.spidle@gmail.com>
2015-09-01 14:07:07 -0500

    Add some get business on the router.

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,21 +1,26 @@
-//$(document).ready(function(){
-//    $.ajax({
-//        type: 'GET',
-//        dataType: 'json',
-//        url: '/memes'
-//    })
-//
-//        .done(function(data){
-//            var haters = data;
-//            console.log(haters);
-//        })
-//
-//        .fail(function(jqXHR, textStatus, errorThrown){
-//            console.log('There was an error');
-//        })
-//
-//        .always(function(){
-//            console.log('AJAX complete');
-//        })
-//
-//});
+$(document).ready(function(){
+
+    $('#addMessage').on('click', function(e){
+        //e.preventDefault();
+
+        $.ajax({
+            type: 'GET',
+            dataType: 'json',
+            url: '/memes/messages'
+        })
+
+            .done(function(data){
+                console.log(data);
+            })
+
+            .fail(function(jqXHR, textStatus, errorThrown){
+                console.log('There was an error');
+            })
+
+            .always(function(){
+                console.log('AJAX complete');
+            })
+
+
+    });
+});
--- routes/memes.js
+++ routes/memes.js
@@ -6,56 +6,56 @@ var json = require('../models/memes');
 var messages =require('../models/messages');
 var parser = require('body-parser');
 
-
-// pulls JSON data
-router.get('/', function(req, res, next){
-    console.log(messages,json);
-    res.render('memes', {title: 'Haters Gonna Hate', haters: json, messages:messages});
-
-});
-
 var file = path.join(__dirname,"../models/messages.JSON");
-var newArray;
 
-fs.readFile(file, 'utf8', function (err, data) {
-    if (err) return console.log(err);
-    var msgJson = JSON.parse(data);
-    console.log(msgJson);
-    newArray = msgJson;
-    console.log(newArray);
-    return newArray;
+// get messages JSON file
+
+var routerGet = router.get('/messages', function(req, res, next){
+
+    fs.readFile(file, 'utf8', function (err, data) {
+        if (err) return console.log(err);
+        var getJson = JSON.parse(data);
+        res.send(getJson);
+    });
 });
 
+// renders page with JSON data
+router.get('/', function(req, res, next){
+
+    fs.readFile(file, 'utf8', function (err, data) {
+        if (err) return console.log(err);
+        var getJson = JSON.parse(data);
+        res.render('memes', {title: 'Haters Gonna Hate', haters: json, messages: getJson});
+    });
+});
+
+// post added content to JSON file
 
 router.post('/', function(req, res, next) {
 
-
-
     console.log("sending message");
-
 
     var id = req.body.id;
     var newMessage = req.body.newMessage;
-    //var obj = {name: id, message: newMessage};
-    //var newJson = msgJson.push(obj);
-    //console.log(newJson);
 
-    for (i = 0; i < newArray.length; i++) {
-
-        if (id == newArray[i].name) {
-            newArray[i].message = newArray[i].message + "\n\n" + newMessage;
-        }
-    }
-
-
-    fs.writeFile(file, JSON.stringify(newArray) , 'utf-8', function (err) {
+    fs.readFile(file, 'utf8', function (err, data) {
         if (err) return console.log(err);
-        console.log('Wrote Data');
+        var newArray = JSON.parse(data);
+
+        for (i = 0; i < newArray.length; i++) {
+
+            if (id == newArray[i].name) {
+                newArray[i].message = newArray[i].message + "\n\n" + newMessage;
+            }
+        }
+
+        fs.writeFile(file, JSON.stringify(newArray) , 'utf-8', function (err) {
+            if (err) return console.log(err);
+            console.log('Wrote Data');
+        });
     });
 
-
-    res.send(id + ", " + newMessage);
-
+    //res.render('memes');
 });
 
 module.exports = router;
